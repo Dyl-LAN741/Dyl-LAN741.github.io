03fee1d2cccb78598067e145fa52c210024297f2
Dyl-LAN741 <Dyl-LAN741@users.noreply.github.com>
2025-07-04 21:20:03 +0000

    deploy: 5391262b41cb08d7850420c02a1ef3905024fd63

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,12 @@ ninja.data = [{
           },
         },{id: "projects-intégration-d-okta-à-aws-fédération-d-identité-provisioning-et-mfa",
           title: 'Intégration d’Okta à AWS : fédération d’identité, provisioning et MFA',
-          description: "Dans ce projet, j’ai intégré Okta à AWS afin de centraliser la gestion des identités et des accès, en appliquant l&#39;authentification unique via la fédération des identités, l&#39;automatisation du provisioning des comptes utilisateurs, et la mise en place du MFA pour les accès aux consoles d&#39;AWS et d&#39;Okta.",
+          description: "Dans ce projet, j’ai intégré un fournisseur d&#39;identité (IdP) à AWS afin de centraliser la gestion des identités et des accès, en appliquant l&#39;authentification unique (SSO) via la fédération des identités, le provisioning des comptes utilisateurs (SCIM), et la mise en place du MFA pour sécuriser les accès aux consoles d&#39;Okta et AWS, dans le but de renforcer la sécurité, simplifier la gestion des accès et d&#39;améliorer l&#39;expérience utilisateur.",
           section: "Projects",handler: () => {
               window.location.href = "/projets/personnels/aws_okta/";
+            },},{id: "projects-supervision-centralisée-des-logs-et-alertes-avec-prometheus-et-grafana",
+          title: 'Supervision centralisée des logs et alertes avec Prometheus et Grafana.',
+          description: "Dans ce projet, j’ai développé un pipeline en Python pour convertir des logs et alertes provenant de différents outils de sécurité (firewall, EDR, XDR) en métriques, pour exposer ces données sur Prometheus afin de générer des dashboards dynamiques depuis Grafana, dans le but de centraliser la supervision des logs sur une même plateforme.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projets/professionnels/prometheus_grafana/";
             },},];
