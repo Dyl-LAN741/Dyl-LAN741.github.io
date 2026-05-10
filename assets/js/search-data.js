b880b61f116aa60d4283c7bbd37c8235057cc2ce
Dyl-LAN741 <Dyl-LAN741@users.noreply.github.com>
2026-05-10 23:54:21 +0000

    deploy: 7c6746323205cda9ee952c7c3ae86cca6a432b8f

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,12 +32,17 @@ ninja.data = [{
           },
         },{id: "projects-intégration-d-39-okta-à-aws-fédération-provisioning-et-mfa",
           title: 'Intégration d&amp;#39;Okta à AWS : fédération, provisioning et MFA',
-          description: "J’ai sécurisé et simplifié la gestion des accès et des identités au sein d&#39;un environnement cloud, en y intégrant un fournisseur d’identité pour assurer l’authentification unique, le provisionnement des utilisateurs, tout en appliquant des mesures de sécurité telles que l&#39;authentification multifacteur et des contrôles d&#39;accès basés sur les rôles.",
+          description: "Résumé : Sécurisation et simplification de la gestion des accès et des identités au sein d&#39;un environnement cloud, intégrant un fournisseur d’identité pour assurer l’authentification unique, le provisionnement des utilisateurs, et appliquant des mesures de sécurité telles que l&#39;authentification multifacteur, des contrôles d&#39;accès basés sur les rôles (RBAC).",
           section: "Projects",handler: () => {
               window.location.href = "/projets/personnels/aws_okta/";
             },},{id: "projects-supervision-centralisée-des-logs-avec-prometheus-et-grafana",
           title: 'Supervision centralisée des logs avec Prometheus et Grafana',
-          description: "J’ai centralisé la supervision des logs provenant de divers outils de sécurité sur une plateforme, en développant un pipeline Python qui convertit ces alertes en métriques stockables dans Prometheus, permettant la création de tableaux de bord dynamiques avec Grafana.",
+          description: "Résumé : Centralisation des logs provenant de divers outils de sécurité (EDR, XDR, NGFW) via un pipeline Python qui convertit ces logs en métriques stockables dans Prometheus, permettant leur visualisation sur des tableaux de bord dynamiques dans Grafana et facilitant ainsi la supervision.",
           section: "Projects",handler: () => {
               window.location.href = "/projets/professionnels/prometheus_grafana/";
+            },},{id: "projects-implémentation-et-attaque-temporelle-sur-rsa",
+          title: 'Implémentation et attaque temporelle sur RSA',
+          description: "Résumé : Implémentation complète de l&#39;algorithme de chiffrement RSA, puis réalisation une attaque par canal auxiliaire (attaque temporelle) pour compromettre le crytosystème et retrouver la clé secrète.",
+          section: "Projects",handler: () => {
+              window.location.href = "/projets/academiques/rsa_side_channel_attack/";
             },},];
